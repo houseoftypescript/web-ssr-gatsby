d8ae8a8ca2ae9e1967e78b2699088eced59b8bbd
hieudoanm <hieumdoan@gmail.com>
2023-04-17 14:19:09 +0000

    Deploying to gh-pages from @ houseoftypescript/gatsby-template@30fef2258b79fe1b2a64e05ba9a02a251d3bf87c 🚀

---
+++ component---src-pages-404-tsx-940ea50ae28b3734dc2e.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_template=self.webpackChunkgatsby_template||[]).push([[218],{2513:function(e,t,a){a.r(t),a.d(t,{Head:function(){return n}});var l=a(7294);t.default=()=>l.createElement("main",{className:"w-full h-screen flex flex-col items-center justify-center gap-4"},l.createElement("h1",{className:"text-9xl font-bold"},"404"),l.createElement("p",{className:"text-3xl uppercase"},"Page Not Found"));const n=()=>l.createElement("title",null,"Gatsby Template - Not found")}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-940ea50ae28b3734dc2e.js.map
